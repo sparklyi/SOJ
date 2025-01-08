f19c3f18cded92d91e5baa5d04ceb404a8c86770
liyi <513254687@qq.com>
2025-01-08 17:34:31 +0800

    user结构设计,临时提交

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,1 +1,16 @@
 package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"type:varchar(255);not null;comment:用户名" json:"username,omitempty"`
+	Password string `gorm:"type:varchar(255);not null;comment:密码" json:"password,omitempty"`
+	Email    string `gorm:"type:varchar(255);not null;unique;comment:邮箱" json:"email,omitempty"`
+	Avatar   string `gorm:"type:varchar(1000);not null;comment:头像链接" json:"avatar,omitempty"`
+	Role     int    `gorm:"type:int;not null;default:1;comment:-1封禁 1用户 2管理员 3超级管理员" json:"role,omitempty"`
+}
+
+func (User) TableName() string {
+	return "user"
+}
