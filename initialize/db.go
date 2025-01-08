56b4ab48b48028b0d04ac7dfc55cfb142fc88dbe
liyi <513254687@qq.com>
2025-01-08 17:34:31 +0800

    user结构设计,临时提交

--- initialize/db.go
+++ initialize/db.go
@@ -1,1 +1,18 @@
 package initialize
+
+import (
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	dsn := viper.GetString("mysql.dsn")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+
+}
