2a8bc3e77a9af2654a00cb020cf304bfcf15fcf1
liyi <513254687@qq.com>
2025-01-09 17:38:20 +0800

    邮件发送

--- internal/api/user.go
+++ internal/api/user.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"SOJ/pkg/email"
 	"SOJ/utils/jwt"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -12,13 +13,15 @@ type UserAPI struct {
 	jwt        *jwt.JWT
 	log        *zap.Logger
 	middleware []gin.HandlerFunc
+	email      *email.Email
 }
 
-func NewUserAPI(log *zap.Logger, jwt *jwt.JWT, fc []gin.HandlerFunc) *UserAPI {
+func NewUserAPI(log *zap.Logger, jwt *jwt.JWT, fc []gin.HandlerFunc, e *email.Email) *UserAPI {
 	return &UserAPI{
 		jwt:        jwt,
 		log:        log,
 		middleware: fc,
+		email:      e,
 	}
 }
 func (u *UserAPI) TestFunc(ctx *gin.Context) {
@@ -38,8 +41,13 @@ func (u *UserAPI) TestFunc(ctx *gin.Context) {
 	fmt.Println(refresh)
 
 	// 长token手动无效，加入redis黑名单
-	err = u.jwt.BanToken(ctx, refresh)
-	if err != nil {
+	//err = u.jwt.BanToken(ctx, refresh)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//发送邮件
+	if err = u.email.Send([]string{"sparkyi@qq.com"}, "test"); err != nil {
 		panic(err)
 	}
 
