61a512728f0c66bf2312bd7baf5faf28d6cacb78
liyi <513254687@qq.com>
2025-01-15 17:26:42 +0800

    结构修改，部分完善

--- internal/api/user.go
+++ internal/api/user.go
@@ -1,56 +1,13 @@
 package api
 
 import (
-	"SOJ/internal/mq"
-	"SOJ/utils/jwt"
-	"fmt"
+	"SOJ/internal/handle"
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
-	"net/http"
 )
 
-type UserAPI struct {
-	jwt        *jwt.JWT
-	log        *zap.Logger
-	middleware []gin.HandlerFunc
-	email      *mq.EmailProducer
-}
-
-func NewUserAPI(log *zap.Logger, jwt *jwt.JWT, fc []gin.HandlerFunc, e *mq.EmailProducer) *UserAPI {
-	return &UserAPI{
-		jwt:        jwt,
-		log:        log,
-		middleware: fc,
-		email:      e,
+func UserRoute(r *gin.RouterGroup, u *handle.UserHandler) {
+	user := r.Group("/user")
+	{
+		user.GET("/test", u.TestFunc)
 	}
-}
-func (u *UserAPI) TestFunc(ctx *gin.Context) {
-
-	//颁发token
-
-	access, refresh, err := u.jwt.CreateToken(ctx, 1, 1)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{})
-		return
-	}
-	//ctx.JSON(http.StatusOK, gin.H{
-	//	"SOJ-Access-Token":  access,
-	//	"SOJ-Refresh-Token": refresh,
-	//})
-	fmt.Println(access)
-	fmt.Println(refresh)
-
-	// 长token手动无效，加入redis黑名单
-	//err = u.jwt.BanToken(ctx, refresh)
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	//发送邮件
-	//if err = u.email.Send([]string{"sparkyi@qq.com"}, "test"); err != nil {
-	//	panic(err)
-	//}
-	c := mq.EmailContent{Content: "test", Target: []string{"513254687@qq.com", "3026080028@qq.com"}}
-	u.email.Send(ctx, c, 20)
-
 }
