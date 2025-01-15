14f8adc5bef93ecccf6541455371156a5a357ac4
liyi <513254687@qq.com>
2025-01-15 17:28:56 +0800

    邮箱接口完善，消息队列完善

--- internal/mq/email_c.go
+++ internal/mq/email_c.go
@@ -4,20 +4,29 @@ import (
 	"SOJ/pkg/email"
 	"context"
 	"encoding/json"
+	"github.com/redis/go-redis/v9"
+	"github.com/streadway/amqp"
 	"go.uber.org/zap"
+	"time"
+)
+
+const (
+	MaxRetry = 5
 )
 
 type EmailConsumer struct {
 	log   *zap.Logger
 	email *email.Email
 	*EmailProducer
+	Rs *redis.Client
 }
 
-func NewEmailConsumer(log *zap.Logger, email *email.Email, p *EmailProducer) *EmailConsumer {
+func NewEmailConsumer(log *zap.Logger, email *email.Email, p *EmailProducer, rs *redis.Client) *EmailConsumer {
 	return &EmailConsumer{
 		log:           log,
 		email:         email,
 		EmailProducer: p,
+		Rs:            rs,
 	}
 
 }
@@ -27,27 +36,43 @@ func (c *EmailConsumer) Consume(ctx context.Context) error {
 	defer c.log.Info("end consume email")
 
 	//从信道中消费消息
-	msgs, err := c.Channel.Consume(c.QueueName, "", false, false, false, false, nil)
+	msgs, err := c.Channel.Consume(c.QueueName, "", true, false, false, false, nil)
 	if err != nil {
 		c.log.Error("consume email fail", zap.Error(err))
 		return err
 	}
 
 	for msg := range msgs {
-		content := EmailContent{}
-		err = json.Unmarshal(msg.Body, &content)
-		if err != nil {
-			c.log.Error("unmarshal email fail", zap.Error(err))
-			msg.Nack(false, false)
-			continue
-		}
-		err = c.email.Send(content.Target, content.Content)
-		if err != nil {
-			c.log.Error("send email fail", zap.Error(err))
-			msg.Nack(false, true)
-		} else {
-			msg.Ack(true)
-		}
+		//开启协程
+		go func(msg amqp.Delivery) {
+			content := EmailContent{}
+			err = json.Unmarshal(msg.Body, &content)
+			if err != nil {
+				c.log.Error("unmarshal email fail", zap.Error(err))
+				return
+			}
+			if len(content.Target) == 0 {
+				c.log.Error("邮件接收方为空", zap.Any("content", content))
+				return
+			}
+
+			for range MaxRetry {
+				//只有验证码类型会令Storage=true
+				if content.Code != "" {
+					if err = c.Rs.Set(ctx, content.Target[0], content.Code, time.Minute).Err(); err != nil {
+						c.log.Error("验证码缓存失败", zap.Error(err))
+						continue
+					}
+				}
+				if err = c.email.Send(content.Target, content.Subject, content.Content); err != nil {
+					continue
+				}
+				//执行到这则已经完成缓存+发送
+				return
+			}
+			//执行到这 即重试5次依然失败，直接丢弃并记日志
+			c.log.Error("消费达到最大次数:", zap.Any("content", content))
+		}(msg)
 	}
 	return nil
 }
--- pkg/email/email.go
+++ pkg/email/email.go
@@ -28,10 +28,12 @@ func New(log *zap.Logger) *Email {
 }
 
 // Send 邮件发送 content可以为html格式
-func (e *Email) Send(addr []string, content string) error {
+func (e *Email) Send(addr []string, subject string, content string) error {
 	m := gomail.NewMessage()
+
 	m.SetHeader("From", e.Username)
 	m.SetHeader("To", addr...)
+	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", content)
 	if err := e.DialAndSend(m); err != nil {
 		e.log.Error("邮件发送失败", zap.Error(err))
