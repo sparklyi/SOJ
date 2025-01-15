c1b727247d06c4e4b110bc97c759a169579269e2
liyi <513254687@qq.com>
2025-01-15 17:26:42 +0800

    结构修改，部分完善

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,1 +1,16 @@
 package entity
+
+type Register struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8,max=20"`
+	Code     string `json:"code" binding:"required,len=6"`
+}
+type LoginByEmail struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8,max=20"`
+	Code     string `json:"code" binding:"required,len=6"`
+}
+type LoginByPassword struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8,max=20"`
+}
