4d163dd196f42863f7858a59ceea0641caabccdd
liyi <513254687@qq.com>
2025-01-17 10:18:15 +0800

    email架构调整分层, 完善错误处理

--- internal/handle/email.go
+++ internal/handle/email.go
@@ -2,36 +2,28 @@ package handle
 
 import (
 	"SOJ/internal/entity"
-	"SOJ/internal/mq"
-	"SOJ/utils"
+	"SOJ/internal/service"
+	"SOJ/utils/response"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type EmailHandler struct {
-	emailProducer *mq.EmailProducer
+	svc *service.EmailService
 }
 
-func NewEmailHandler(e *mq.EmailProducer) *EmailHandler {
-	return &EmailHandler{e}
+func NewEmailHandler(se *service.EmailService) *EmailHandler {
+	return &EmailHandler{se}
 }
 
 func (e *EmailHandler) SendEmailCode(c *gin.Context) {
 	req := entity.SendEmailCode{}
 	if err := c.ShouldBind(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"msg": "参数无效"})
+		response.BadRequestErrorWithMsg(c, "参数无效")
 		return
 	}
-	code := utils.GenerateRandCode(6)
-	data := mq.EmailContent{
-		Target:  []string{req.Email},
-		Subject: "注册验证",
-		Content: `<h1>欢迎注册SOJ!</h1><br>你的注册码为<a>` + code + `</a><br>验证码有效时限为1分钟,请勿泄露于他人!`,
-		Code:    code,
+	if err := e.svc.SendEmailCode(c, &req); err != nil {
+		response.BadRequestErrorWithMsg(c, err.Error())
 	}
-	err := e.emailProducer.Send(c, data, 10)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"msg": "验证码发送失败"})
-	}
+	response.SuccessNoContent(c)
 
 }
