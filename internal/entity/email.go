d5b3ef2ac154372a5bf3908bf0de1116922bfc03
li <513254687@qq.com>
2025-01-19 21:53:50 +0800

    upd

--- internal/entity/email.go
+++ internal/entity/email.go
@@ -1,5 +1,13 @@
 package entity
 
+import "time"
+
 type SendEmailCode struct {
 	Email string `json:"email" binding:"required,email"`
+}
+type SendEmail struct {
+	Recipients []string   `json:"recipients" binding:"required,email"`
+	Subject    string     `json:"subject" binding:"required"`
+	Body       string     `json:"body" binding:"required"`
+	SendTime   *time.Time `json:"delay" binding:"omitempty"`
 }
