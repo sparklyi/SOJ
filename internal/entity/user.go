20cdeef2085c3fcd61f327469f965f604e7c89ce
liyi <513254687@qq.com>
2025-01-21 17:32:44 +0800

    完善user功能

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -6,11 +6,22 @@ type Register struct {
 	Code     string `json:"code" binding:"required,len=6"`
 }
 type LoginByEmail struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=8,max=20"`
-	Code     string `json:"code" binding:"required,len=6"`
+	Email string `json:"email" binding:"required,email"`
+	Code  string `json:"code" binding:"required,len=6"`
 }
 type LoginByPassword struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=8,max=20"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,min=8,max=20"`
+	CaptchaID string `json:"captcha_id" binding:"required"`
+	Captcha   string `json:"captcha" binding:"required,len=6"`
+}
+
+type UserInfo struct {
+	ID       int    `json:"id" binding:"omitempty,number"`
+	Username string `json:"username" binding:"omitempty"`
+	Email    string `json:"email" binding:"omitempty,email"`
+	Tel      string `json:"tel" binding:"omitempty"`
+	Role     int    `json:"role" binding:"omitempty,oneof=-1 1 2 3"`
+	Page     int    `json:"page" binding:"omitempty,number,min=1"`
+	PageSize int    `json:"page_size" binding:"omitempty,number,min=20,max=100"`
 }
--- internal/service/user.go
+++ internal/service/user.go
@@ -4,13 +4,18 @@ import (
 	"SOJ/internal/entity"
 	"SOJ/internal/model"
 	"SOJ/internal/repository"
+	"SOJ/utils"
 	"crypto/sha512"
 	"errors"
 	"fmt"
 	"github.com/anaskhan96/go-password-encoder"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"github.com/tencentyun/cos-go-sdk-v5"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
+	"mime/multipart"
+	"strconv"
 	"strings"
 )
 
@@ -18,13 +23,15 @@ type UserService struct {
 	log  *zap.Logger
 	repo *repository.UserRepository
 	rs   *redis.Client
+	cs   *cos.Client
 }
 
-func NewUserService(log *zap.Logger, repo *repository.UserRepository, rs *redis.Client) *UserService {
+func NewUserService(log *zap.Logger, repo *repository.UserRepository, rs *redis.Client, cs *cos.Client) *UserService {
 	return &UserService{
 		log:  log,
 		repo: repo,
 		rs:   rs,
+		cs:   cs,
 	}
 }
 
@@ -43,15 +50,22 @@ func (us *UserService) CheckPassword(raw, encrypt string) bool {
 	return password.Verify(raw, list[1], list[2], option)
 }
 
+func (us *UserService) CheckCode(ctx *gin.Context, email string, code string) (bool, error) {
+	check, err := us.rs.Get(ctx, email).Result()
+	if err != nil && !errors.Is(err, redis.Nil) {
+		us.log.Error("redis获取失败", zap.Error(err))
+		return false, err
+	}
+	return check == code, nil
+
+}
+
 // Register 注册
 func (us *UserService) Register(ctx *gin.Context, req *entity.Register) (*model.User, error) {
 
-	check, err := us.rs.Get(ctx, req.Email).Result()
-	//redis中不存在此键时返回redis.Nil
-	if err != nil && !errors.Is(err, redis.Nil) {
+	if check, err := us.CheckCode(ctx, req.Email, req.Code); err != nil {
 		return nil, err
-	}
-	if check != req.Code {
+	} else if !check {
 		return nil, errors.New("验证码错误")
 	}
 
@@ -60,8 +74,54 @@ func (us *UserService) Register(ctx *gin.Context, req *entity.Register) (*model.
 		Password: us.EncryptPassword(req.Password),
 	}
 	//转repository层
-	if err = us.repo.Register(ctx, &user); err != nil {
+	if err := us.repo.CreateUserByEmail(ctx, &user); err != nil {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (us *UserService) LoginByEmail(ctx *gin.Context, req *entity.LoginByEmail) (*model.User, error) {
+	if check, err := us.CheckCode(ctx, req.Email, req.Code); err != nil {
+		return nil, err
+	} else if !check {
+		return nil, errors.New("验证码错误")
+	}
+	return us.repo.GetUserByEmail(ctx, req.Email)
+}
+
+func (us *UserService) GetUserByID(ctx *gin.Context, id int) (*model.User, error) {
+	user, err := us.repo.GetUserByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	//去敏
+	user.Password = ""
+	user.Tel = ""
+	user.Email = ""
+	return user, nil
+}
+
+func (us *UserService) UploadAvatar(ctx *gin.Context, fh *multipart.FileHeader, ID int) error {
+	f, err := fh.Open()
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	t := strings.Split(fh.Filename, ".")
+	if len(t) != 2 && t[1] != "jpg" && t[1] != "jpeg" && t[1] != "png" {
+		return errors.New("文件不符")
+	}
+	t[0] = utils.CryptoSHA1(strconv.Itoa(ID))
+	name := "avatar/" + t[0] + "." + t[1]
+	_, err = us.cs.Object.Put(ctx, name, f, nil)
+	if err != nil {
+		us.log.Error("上传失败", zap.Error(err))
+		return errors.New("上传失败")
+	}
+	//将头像地址存入数据库
+	user := model.User{
+		Model:  gorm.Model{ID: uint(ID)},
+		Avatar: us.cs.BaseURL.BucketURL.String() + "/" + name,
+	}
+	return us.repo.UpdateUserByID(ctx, &user)
 }
