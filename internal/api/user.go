e4116d1926c7ccff7c482b2dffda9be82a8dfd51
liyi <513254687@qq.com>
2025-01-21 17:32:44 +0800

    完善user功能

--- internal/api/user.go
+++ internal/api/user.go
@@ -9,5 +9,10 @@ func UserRoute(r *gin.RouterGroup, u *handle.UserHandler, mid []gin.HandlerFunc)
 	user := r.Group("/user")
 	{
 		user.POST("/register", u.Register)
+		user.POST("/login_email", u.LoginByEmail)
+		user.POST("/refresh_token", u.RefreshToken)
+		user.GET("/logout", u.Logout)
+		user.GET("/:id", u.GetUserInfo)
+		user.POST("/avatar", mid[1], u.UploadAvatar)
 	}
 }
--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,7 +1,9 @@
 package repository
 
 import (
+	"SOJ/internal/entity"
 	"SOJ/internal/model"
+	"SOJ/utils"
 	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
@@ -20,6 +22,7 @@ type UserRepository struct {
 	rs  *redis.Client
 }
 
+// NewUserRepository 依赖注入
 func NewUserRepository(log *zap.Logger, db *gorm.DB, rs *redis.Client) *UserRepository {
 	return &UserRepository{
 		log: log,
@@ -28,8 +31,9 @@ func NewUserRepository(log *zap.Logger, db *gorm.DB, rs *redis.Client) *UserRepo
 	}
 }
 
-func (ur *UserRepository) Register(c *gin.Context, user *model.User) error {
-	err := ur.db.Create(user).Error
+// CreateUserByEmail 使用邮箱注册用户
+func (ur *UserRepository) CreateUserByEmail(c *gin.Context, user *model.User) error {
+	err := ur.db.WithContext(c).Create(user).Error
 	var mysqlErr *mysql.MySQLError
 
 	if errors.As(err, &mysqlErr) && mysqlErr.Number == MysqlDuplicateError {
@@ -40,4 +44,90 @@ func (ur *UserRepository) Register(c *gin.Context, user *model.User) error {
 	}
 	return nil
 
+}
+
+// GetUserByEmail 根据邮箱获取用户信息
+func (ur *UserRepository) GetUserByEmail(c *gin.Context, email string) (*model.User, error) {
+	user := &model.User{}
+	err := ur.db.WithContext(c).Where("email = ?", email).First(user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errors.New("此邮箱未注册")
+	} else if err != nil {
+		ur.log.Error("数据库获取数据失败", zap.Error(err))
+		return nil, errors.New("内部错误")
+	}
+	return user, nil
+}
+
+// GetUserByID 根据id获取用户信息
+func (ur *UserRepository) GetUserByID(c *gin.Context, id int) (*model.User, error) {
+	user := &model.User{}
+	err := ur.db.WithContext(c).Where("id = ?", id).First(user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errors.New("ID不存在")
+	} else if err != nil {
+		ur.log.Error("数据库获取数据失败", zap.Error(err))
+		return nil, errors.New("内部错误")
+	}
+	return user, nil
+}
+
+// UpdateUserByID 通过id更新用户信息
+func (ur *UserRepository) UpdateUserByID(c *gin.Context, user *model.User) error {
+	var mysqlErr *mysql.MySQLError
+	err := ur.db.WithContext(c).Updates(user).Error
+	if errors.As(err, &mysqlErr) && mysqlErr.Number == MysqlDuplicateError {
+		return errors.New("唯一索引冲突")
+	} else if err != nil {
+		ur.log.Error("数据库获取数据失败", zap.Error(err))
+		return errors.New("内部错误")
+	}
+	return nil
+}
+
+// DeleteUserByID 根据id删除用户
+func (ur *UserRepository) DeleteUserByID(c *gin.Context, id int) error {
+	err := ur.db.WithContext(c).Where("id = ?", id).Delete(&model.User{}).Error
+	if err != nil {
+		ur.log.Error("数据库异常", zap.Error(err))
+		return errors.New("内部错误")
+	}
+	return nil
+}
+
+// DeleteUserByEmail 根据邮箱删除用户
+func (ur *UserRepository) DeleteUserByEmail(c *gin.Context, email string) error {
+	err := ur.db.WithContext(c).Where("email = ?", email).Delete(&model.User{}).Error
+	if err != nil {
+		ur.log.Error("数据库异常", zap.Error(err))
+		return errors.New("内部错误")
+	}
+	return nil
+}
+
+// GetUserList 获取用户列表(条件分页)
+func (ur *UserRepository) GetUserList(c *gin.Context, user *entity.UserInfo) (*[]model.User, error) {
+	db := ur.db.WithContext(c).Model(&model.User{})
+	if user.ID != 0 {
+		db = db.Where("id = ?", user.ID)
+	}
+	if user.Email != "" {
+		db = db.Where("email = ?", user.Email)
+	}
+	if user.Tel != "" {
+		db = db.Where("tel = ?", user.Tel)
+	}
+	if user.Username != "" {
+		db = db.Where("username LIKE ?", "%"+user.Username+"%")
+	}
+	if user.Role != 0 {
+		db = db.Where("role = ?", user.Role)
+	}
+	var us []model.User
+	err := db.Scopes(utils.Paginate(user.Page, user.PageSize)).Find(&us).Error
+	if err != nil {
+		ur.log.Error("获取用户列表失败", zap.Error(err))
+		return nil, errors.New("内部错误")
+	}
+	return &us, nil
 }
