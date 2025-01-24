3b990e16302d561dcb9833fd54e35ccde5535885
liyi <513254687@qq.com>
2025-01-24 09:05:52 +0800

    修复邮件重复发送bug，删除多余api

--- internal/service/email.go
+++ internal/service/email.go
@@ -25,13 +25,13 @@ func NewEmailService(log *zap.Logger, rs *redis.Client, e *mq.EmailProducer) *Em
 	}
 }
 
-func (es *EmailService) SendRegisterCode(ctx *gin.Context, req *entity.SendEmailCode) error {
+func (es *EmailService) SendVerifyCode(ctx *gin.Context, req *entity.SendEmailCode) error {
 	code := utils.GenerateRandCode(6)
 	fmt.Println(code)
 	data := mq.EmailContent{
 		Target:  []string{req.Email},
-		Subject: "注册验证",
-		Content: `<h1>欢迎注册SOJ!</h1><br>你的注册码为<a>` + code + `</a><br>验证码有效时限为1分钟,请勿泄露于他人!`,
+		Subject: "验证码",
+		Content: `<h1>SOJ</h1><br>你的验证码为<a>` + code + `</a><br>验证码有效时限为1分钟,请勿泄露于他人!`,
 		Code:    code,
 	}
 	//redis是否已经缓存过验证码
@@ -39,32 +39,7 @@ func (es *EmailService) SendRegisterCode(ctx *gin.Context, req *entity.SendEmail
 	if err != nil {
 		return err
 	}
-	if exist == 0 {
-		return errors.New("验证码未过期")
-	}
-	err = es.emailProducer.Send(ctx, data, 0)
-	if err != nil {
-		es.log.Error("生产者消息生产失败", zap.Error(err))
-		return errors.New("验证码发送失败")
-	}
-	return nil
-}
-
-func (es *EmailService) SendResetPwdCode(ctx *gin.Context, req *entity.SendEmailCode) error {
-	code := utils.GenerateRandCode(6)
-	fmt.Println(code)
-	data := mq.EmailContent{
-		Target:  []string{req.Email},
-		Subject: "密码重置",
-		Content: `你的验证码为<a>` + code + `</a><br>验证码有效时限为1分钟,请勿泄露于他人!`,
-		Code:    code,
-	}
-	//redis是否已经缓存过验证码
-	exist, err := es.rs.Exists(ctx, req.Email).Result()
-	if err != nil {
-		return err
-	}
-	if exist == 0 {
+	if exist != 0 {
 		return errors.New("验证码未过期")
 	}
 	err = es.emailProducer.Send(ctx, data, 0)
