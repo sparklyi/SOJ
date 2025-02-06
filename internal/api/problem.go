359424510fb525e31e1f13e1ab21891357265a65
liyi <513254687@qq.com>
2025-02-06 17:32:39 +0800

    题目api完善-1

--- internal/api/problem.go
+++ internal/api/problem.go
@@ -8,11 +8,15 @@ import (
 func ProblemRoute(r *gin.RouterGroup, p *handle.ProblemHandle, mid []gin.HandlerFunc) {
 	problem := r.Group("problem")
 	{
-		problem.GET("/list")
-		problem.GET("/:pid")
-		problem.POST("/create", mid[1], mid[2])
+		//problem.GET("/set/:tag")
+		problem.GET("/total", mid[1], p.Count)
+		problem.GET("/:pid", mid[1], p.Detail)
+		problem.GET("/:pid/submission", mid[1])
+		problem.POST("/", mid[1], p.List)
+		problem.POST("/create", mid[1], mid[2], p.Create)
 		problem.PUT("/update", mid[1], mid[2])
 		problem.DELETE("/delete", mid[1], mid[2])
 		problem.POST("/search", mid[1])
+
 	}
 }
