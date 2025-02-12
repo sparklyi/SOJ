4e78e0a900a5fa594e7f8a9dfd7259d28946dee4
liyi <513254687@qq.com>
2025-02-12 17:33:24 +0800

    自测运行功能完善

---
+++ internal/constant/judge.go
@@ -0,0 +1,26 @@
+package constant
+
+const (
+	JudgePending = iota
+	JudgeInQueue
+	JudgeAccepted
+	Judge
+)
+
+var tab = []string{
+	"Pending",
+	"In Queue",
+	"Processing",
+	"Accepted",
+	"Wrong Answer",
+	"Time Limit Exceeded",
+	"Compilation Error",
+	"Runtime Error (SIGSEGV)",
+	"Runtime Error (SIGXFSZ)",
+	"Runtime Error (SIGFPE)",
+	"Runtime Error (SIGABRT)",
+	"Runtime Error (NZEC)",
+	"Runtime Error",
+	"Internal Error",
+	"Exec Format Error",
+}
