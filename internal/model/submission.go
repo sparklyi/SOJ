c343c608fd7f8a8c83a79d81c4bb4f1dae170387
liyi <513254687@qq.com>
2025-02-15 13:48:22 +0800

    完善测评表结构，增加部分逻辑

--- internal/model/submission.go
+++ internal/model/submission.go
@@ -4,17 +4,20 @@ import "gorm.io/gorm"
 
 type Submission struct {
 	gorm.Model
-	UserID     uint    `json:"user_id,omitempty"`
-	ProblemID  uint    `json:"problem_id,omitempty"`
-	LanguageID uint    `json:"language_id,omitempty"`
-	ContestID  uint    `gorm:"default:0;0表示非比赛提交" json:"contest_id,omitempty"`
-	SourceCode string  `gorm:"type:longtext;comment:源代码'" json:"source_code,omitempty"`
-	Status     string  `gorm:"type:varchar(255);comment:测评状态'" json:"status,omitempty"`
-	Time       float64 `gorm:"comment:测评耗时" json:"time,omitempty"`
-	Memory     float64 `gorm:"comment:测评耗存" json:"memory,omitempty"`
-	Stderr     string  `gorm:"type:longtext;comment:标准错误'" json:"stderr,omitempty"`
-	CompileOut string  `gorm:"type:longtext;comment:编译输出'" json:"compile_out,omitempty"`
-	Visible    *bool   `gorm:"default:true;comment:是否可见" json:"visible,omitempty"`
+	UserID      uint    `json:"user_id,omitempty"`
+	UserName    string  `json:"user_name,omitempty"`
+	ProblemID   uint    `json:"problem_id,omitempty"`
+	ProblemName string  `json:"problem_name,omitempty"`
+	LanguageID  uint    `json:"language_id,omitempty"`
+	Language    string  `json:"language,omitempty"`
+	ContestID   uint    `gorm:"default:0;0表示非比赛提交" json:"contest_id,omitempty"`
+	SourceCode  string  `gorm:"type:longtext;comment:源代码'" json:"source_code,omitempty"`
+	Status      string  `gorm:"type:varchar(255);comment:测评状态'" json:"status,omitempty"`
+	Time        float64 `gorm:"comment:测评耗时" json:"time,omitempty"`
+	Memory      float64 `gorm:"comment:测评耗存" json:"memory,omitempty"`
+	Stderr      string  `gorm:"type:longtext;comment:标准错误'" json:"stderr,omitempty"`
+	CompileOut  string  `gorm:"type:longtext;comment:编译输出'" json:"compile_out,omitempty"`
+	Visible     *bool   `gorm:"default:true;comment:是否可见" json:"visible,omitempty"`
 }
 
 func (Submission) TableName() string {
