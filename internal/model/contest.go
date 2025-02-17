797bff247bafb3ec7597b9ad59220e973fb57bc7
liyi <513254687@qq.com>
2025-02-17 17:33:41 +0800

    比赛模块结构搭建

--- internal/model/contest.go
+++ internal/model/contest.go
@@ -1,14 +1,27 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Contest struct {
 	gorm.Model
-	//存到mongo
-	ObjectID string `gorm:"varchar(30);unique;comment:对应存储在mongo的竞赛id" json:"object_id"`
+	Name        string     `gorm:"type:varchar(255);not null;comment:竞赛名" json:"name,omitempty"`
+	UserID      uint       `json:"user_id,omitempty"`
+	Tag         int        `gorm:"comment:竞赛级别" json:"tag,omitempty"`
+	Type        int        `gorm:"comment:竞赛模式" json:"type,omitempty"`
+	Sponsor     string     `gorm:"comment:主办方" json:"sponsor,omitempty"`
+	Description string     `gorm:"type:text;comment:比赛简介" json:"description,omitempty"`
+	ProblemSet  string     `gorm:"type:json;comment:题目集合" json:"problem_set,omitempty"`
+	Public      *bool      `gorm:"default:true;comment:是否公开比赛" json:"public,omitempty"`
+	Code        string     `gorm:"comment:私人比赛邀请码" json:"code,omitempty"`
+	StartTime   *time.Time `json:"start_time,omitempty"`
+	EndTime     *time.Time `json:"end_time,omitempty"`
+	FreezeTime  *time.Time `json:"freeze_time,omitempty"`
 
 	//外键
-	Apply []Apply `gorm:"constraint:OnUpdate:CASCADE;OnDelete:SET NULL;" json:"apply,omitempty"`
+	Apply []Apply `gorm:"constraint:OnUpdate:CASCADE;OnDelete:SET NULL;" json:"apply,omitempty" `
 }
 
 func (Contest) TableName() string {
