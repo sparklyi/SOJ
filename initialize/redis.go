239202a5e049910c8b328b50e858fe2136869349
liyi <513254687@qq.com>
2025-02-21 09:11:15 +0800

    redis连接检查

--- initialize/redis.go
+++ initialize/redis.go
@@ -1,14 +1,24 @@
 package initialize
 
 import (
+	"context"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
+	"time"
 )
 
 func InitRedis() *redis.Client {
-	return redis.NewClient(&redis.Options{
+	r := redis.NewClient(&redis.Options{
 		Addr:     viper.GetString("redis.addr"),
 		Password: viper.GetString("redis.password"),
 		DB:       viper.GetInt("redis.db"),
 	})
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	//测试redis是否连通
+	if err := r.Ping(ctx).Err(); err != nil {
+		panic(err)
+	}
+	return r
+
 }
