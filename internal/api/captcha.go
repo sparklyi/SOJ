aedfa08e1faa91a6ac4b006ec38f0410a23d031a
liyi <513254687@qq.com>
2025-02-21 09:22:11 +0800

    api添加注释

--- internal/api/captcha.go
+++ internal/api/captcha.go
@@ -8,6 +8,6 @@ import (
 func CaptchaRoute(r *gin.RouterGroup, c *handle.CaptchaHandle, mid []gin.HandlerFunc) {
 	email := r.Group("/captcha")
 	{
-		email.POST("/create", c.GenerateCaptcha)
+		email.POST("/create", c.GenerateCaptcha) //图形验证码创建
 	}
 }
--- internal/api/user.go
+++ internal/api/user.go
@@ -8,17 +8,17 @@ import (
 func UserRoute(r *gin.RouterGroup, u *handle.UserHandle, mid []gin.HandlerFunc) {
 	user := r.Group("/user")
 	{
-		user.GET("/logout", u.Logout)
-		user.GET("/:id", mid[1], u.GetUserInfo)
-		user.POST("/register", u.Register)
-		user.POST("/login_password", u.LoginByPassword)
-		user.POST("/login_email", u.LoginByEmail)
-		user.POST("/refresh_token", u.RefreshToken)
-		user.POST("/avatar", mid[1], u.UploadAvatar)
-		user.POST("/list", mid[1], mid[3], u.GetUserList)
-		user.PUT("/update_password", mid[1], mid[3], u.UpdatePassword)
-		user.PUT("/update", mid[1], u.UpdateUserInfo)
-		user.PUT("/:email", mid[1], mid[3], u.ResetPassword)
-		user.DELETE("/:id", mid[1], mid[3], u.Delete)
+		user.GET("/logout", u.Logout)                                  //退出
+		user.GET("/:id", mid[1], u.GetUserInfo)                        //获取用户信息
+		user.POST("/register", u.Register)                             //注册
+		user.POST("/login_password", u.LoginByPassword)                //密码登录
+		user.POST("/login_email", u.LoginByEmail)                      //邮箱登录
+		user.POST("/refresh_token", u.RefreshToken)                    //令牌刷新
+		user.POST("/avatar", mid[1], u.UploadAvatar)                   //头像上传
+		user.POST("/list", mid[1], mid[3], u.GetUserList)              //用户列表
+		user.PUT("/update_password", mid[1], mid[3], u.UpdatePassword) //密码更新
+		user.PUT("/update", mid[1], u.UpdateUserInfo)                  //用户信息更新
+		user.PUT("/:email", mid[1], mid[3], u.ResetPassword)           //重置密码
+		user.DELETE("/:id", mid[1], mid[3], u.Delete)                  //用户删除
 	}
 }
