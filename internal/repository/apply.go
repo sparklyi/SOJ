0f82be090c0ac627291ccbd27a5d9af70088c623
liyi <513254687@qq.com>
2025-02-25 16:16:14 +0800

    更新repo层上下文参数为context.Context

--- internal/repository/apply.go
+++ internal/repository/apply.go
@@ -5,22 +5,22 @@ import (
 	"SOJ/internal/entity"
 	"SOJ/internal/model"
 	"SOJ/utils"
+	"context"
 	"errors"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 type ApplyRepository interface {
-	CreateApply(ctx *gin.Context, apply *model.Apply) error
-	UpdateApply(ctx *gin.Context, apply *model.Apply) error
-	DeleteApply(ctx *gin.Context, aid int) error
-	GetListByUserID(ctx *gin.Context, uid int, page int, pageSize int) ([]*model.Apply, error)
-	GetList(ctx *gin.Context, req *entity.ApplyList) ([]*model.Apply, error)
-	GetInfoByUserAndContest(ctx *gin.Context, uid uint, cid uint) (*model.Apply, error)
-	GetInfoByID(ctx *gin.Context, id int) (*model.Apply, error)
-	DeleteApplyByContestID(ctx *gin.Context, cid int) error
+	CreateApply(ctx context.Context, apply *model.Apply) error
+	UpdateApply(ctx context.Context, apply *model.Apply) error
+	DeleteApply(ctx context.Context, aid int) error
+	GetListByUserID(ctx context.Context, uid int, page int, pageSize int) ([]*model.Apply, error)
+	GetList(ctx context.Context, req *entity.ApplyList) ([]*model.Apply, error)
+	GetInfoByUserAndContest(ctx context.Context, uid uint, cid uint) (*model.Apply, error)
+	GetInfoByID(ctx context.Context, id int) (*model.Apply, error)
+	DeleteApplyByContestID(ctx context.Context, cid int) error
 }
 
 type apply struct {
@@ -36,7 +36,7 @@ func NewApplyRepository(log *zap.Logger, db *gorm.DB) ApplyRepository {
 }
 
 // CreateApply 创建报名
-func (ar *apply) CreateApply(ctx *gin.Context, apply *model.Apply) error {
+func (ar *apply) CreateApply(ctx context.Context, apply *model.Apply) error {
 	err := ar.db.WithContext(ctx).Create(apply).Error
 	if err != nil {
 		ar.log.Error("报名失败", zap.Error(err), zap.Any("apply info", apply))
@@ -46,7 +46,7 @@ func (ar *apply) CreateApply(ctx *gin.Context, apply *model.Apply) error {
 }
 
 // UpdateApply 更新报名
-func (ar *apply) UpdateApply(ctx *gin.Context, apply *model.Apply) error {
+func (ar *apply) UpdateApply(ctx context.Context, apply *model.Apply) error {
 	err := ar.db.WithContext(ctx).Updates(apply).Error
 	if err != nil {
 		ar.log.Error("报名信息更新失败", zap.Error(err), zap.Any("apply info", apply))
@@ -56,7 +56,7 @@ func (ar *apply) UpdateApply(ctx *gin.Context, apply *model.Apply) error {
 }
 
 // DeleteApply 取消报名
-func (ar *apply) DeleteApply(ctx *gin.Context, aid int) error {
+func (ar *apply) DeleteApply(ctx context.Context, aid int) error {
 	err := ar.db.WithContext(ctx).Delete(&model.Apply{}, aid).Error
 	if err != nil {
 		ar.log.Error("取消报名失败", zap.Error(err), zap.Any("apply id", aid))
@@ -66,7 +66,7 @@ func (ar *apply) DeleteApply(ctx *gin.Context, aid int) error {
 }
 
 // GetListByUserID 获取用户报名信息
-func (ar *apply) GetListByUserID(ctx *gin.Context, uid int, page, pageSize int) ([]*model.Apply, error) {
+func (ar *apply) GetListByUserID(ctx context.Context, uid int, page, pageSize int) ([]*model.Apply, error) {
 	var a []*model.Apply
 	err := ar.db.WithContext(ctx).Scopes(utils.Paginate(page, pageSize)).Where("user_id = ?", uid).Find(&a).Error
 	if err != nil {
@@ -77,7 +77,7 @@ func (ar *apply) GetListByUserID(ctx *gin.Context, uid int, page, pageSize int) 
 }
 
 // GetList 获取报名列表
-func (ar *apply) GetList(ctx *gin.Context, req *entity.ApplyList) ([]*model.Apply, error) {
+func (ar *apply) GetList(ctx context.Context, req *entity.ApplyList) ([]*model.Apply, error) {
 	db := ar.db.WithContext(ctx)
 	if req.ID != 0 {
 		db = db.Where("id = ?", req.ID)
@@ -99,7 +99,7 @@ func (ar *apply) GetList(ctx *gin.Context, req *entity.ApplyList) ([]*model.Appl
 }
 
 // GetInfoByUserAndContest 根据用户id和比赛id获取报名详情
-func (ar *apply) GetInfoByUserAndContest(ctx *gin.Context, uid, cid uint) (*model.Apply, error) {
+func (ar *apply) GetInfoByUserAndContest(ctx context.Context, uid, cid uint) (*model.Apply, error) {
 	a := &model.Apply{}
 	err := ar.db.WithContext(ctx).Where("user_id = ? AND contest_id = ?", uid, cid).First(a).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
@@ -112,7 +112,7 @@ func (ar *apply) GetInfoByUserAndContest(ctx *gin.Context, uid, cid uint) (*mode
 }
 
 // GetInfoByID 根据报名id获取详情
-func (ar *apply) GetInfoByID(ctx *gin.Context, id int) (*model.Apply, error) {
+func (ar *apply) GetInfoByID(ctx context.Context, id int) (*model.Apply, error) {
 	a := &model.Apply{}
 	err := ar.db.WithContext(ctx).Where("id = ?", id).First(a).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
@@ -125,7 +125,7 @@ func (ar *apply) GetInfoByID(ctx *gin.Context, id int) (*model.Apply, error) {
 }
 
 // DeleteApplyByContestID 删除所有cid的报名信息
-func (ar *apply) DeleteApplyByContestID(ctx *gin.Context, cid int) error {
+func (ar *apply) DeleteApplyByContestID(ctx context.Context, cid int) error {
 	err := ar.db.WithContext(ctx).Where("contest_id = ?", cid).Delete(&model.Apply{}).Error
 	if err != nil {
 		ar.log.Error("删除记录失败", zap.Error(err), zap.Any("contest_id", cid))
