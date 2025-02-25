78c7ab4378e92f1618eb5b03c9c301e7e0c2437c
liyi <513254687@qq.com>
2025-02-25 16:16:14 +0800

    更新repo层上下文参数为context.Context

--- internal/repository/language.go
+++ internal/repository/language.go
@@ -7,7 +7,6 @@ import (
 	"SOJ/utils"
 	"context"
 	"errors"
-	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
@@ -15,13 +14,13 @@ import (
 )
 
 type LanguageRepository interface {
-	Create(ctx *gin.Context, lang *model.Language) (*model.Language, error)
-	Update(ctx *gin.Context, lang *model.Language) error
-	Delete(ctx *gin.Context, id int) error
-	GetByID(ctx *gin.Context, id int) (*model.Language, error)
-	GetLangList(ctx *gin.Context, req *entity.Language) ([]*model.Language, error)
-	Count(ctx *gin.Context) (int64, error)
-	GetTransaction(ctx *gin.Context) *gorm.DB
+	Create(ctx context.Context, lang *model.Language) (*model.Language, error)
+	Update(ctx context.Context, lang *model.Language) error
+	Delete(ctx context.Context, id int) error
+	GetByID(ctx context.Context, id int) (*model.Language, error)
+	GetLangList(ctx context.Context, req *entity.Language) ([]*model.Language, error)
+	Count(ctx context.Context) (int64, error)
+	GetTransaction(ctx context.Context) *gorm.DB
 	SyncLanguages(ctx context.Context) error
 }
 type language struct {
@@ -45,7 +44,7 @@ func NewLanguageRepository(log *zap.Logger, db *gorm.DB) LanguageRepository {
 }
 
 // Create 语言增加
-func (lr *language) Create(ctx *gin.Context, lang *model.Language) (*model.Language, error) {
+func (lr *language) Create(ctx context.Context, lang *model.Language) (*model.Language, error) {
 	err := lr.db.WithContext(ctx).Create(lang).Error
 	if err != nil {
 		lr.log.Error("create language error", zap.Error(err))
@@ -55,7 +54,7 @@ func (lr *language) Create(ctx *gin.Context, lang *model.Language) (*model.Langu
 }
 
 // Update 语言更新(名字 是否可用)
-func (lr *language) Update(ctx *gin.Context, lang *model.Language) error {
+func (lr *language) Update(ctx context.Context, lang *model.Language) error {
 	err := lr.db.Debug().WithContext(ctx).Updates(lang).Error
 	if err != nil {
 		lr.log.Error("update language error", zap.Error(err))
@@ -65,7 +64,7 @@ func (lr *language) Update(ctx *gin.Context, lang *model.Language) error {
 }
 
 // Delete 删除语言(不可用 语言删除只能手动去docker环境)
-func (lr *language) Delete(ctx *gin.Context, id int) error {
+func (lr *language) Delete(ctx context.Context, id int) error {
 	err := lr.db.WithContext(ctx).Delete(&model.Language{}, id).Error
 	if err != nil {
 		lr.log.Error("delete language error", zap.Error(err))
@@ -75,7 +74,7 @@ func (lr *language) Delete(ctx *gin.Context, id int) error {
 }
 
 // GetByID 根据id获取语言信息
-func (lr *language) GetByID(ctx *gin.Context, id int) (*model.Language, error) {
+func (lr *language) GetByID(ctx context.Context, id int) (*model.Language, error) {
 	var lang model.Language
 	err := lr.db.WithContext(ctx).First(&lang, id).Error
 	if err != nil {
@@ -86,7 +85,7 @@ func (lr *language) GetByID(ctx *gin.Context, id int) (*model.Language, error) {
 }
 
 // GetLangList 获取语言列表
-func (lr *language) GetLangList(ctx *gin.Context, req *entity.Language) ([]*model.Language, error) {
+func (lr *language) GetLangList(ctx context.Context, req *entity.Language) ([]*model.Language, error) {
 	var langs []*model.Language
 	db := lr.db.WithContext(ctx).Model(&model.Language{})
 	if req.Name != "" {
@@ -108,7 +107,7 @@ func (lr *language) GetLangList(ctx *gin.Context, req *entity.Language) ([]*mode
 }
 
 // Count 测评语言统计
-func (lr *language) Count(ctx *gin.Context) (int64, error) {
+func (lr *language) Count(ctx context.Context) (int64, error) {
 	var count int64
 	err := lr.db.WithContext(ctx).Model(&model.Language{}).Count(&count).Error
 	if err != nil {
@@ -119,7 +118,7 @@ func (lr *language) Count(ctx *gin.Context) (int64, error) {
 }
 
 // GetTransaction 获取事务
-func (lr *language) GetTransaction(ctx *gin.Context) *gorm.DB {
+func (lr *language) GetTransaction(ctx context.Context) *gorm.DB {
 	return lr.db.WithContext(ctx).Begin()
 }
 
