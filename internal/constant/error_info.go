d35b3c247f7596d149534fe5a0bcff4028bf814e
li <513254687@qq.com>
2025-03-02 20:18:43 +0800

    提示信息常量化

--- internal/constant/error_info.go
+++ internal/constant/error_info.go
@@ -4,9 +4,11 @@ const (
 	ServerError       = "服务器异常"
 	ParamError        = "参数错误"
 	CodeError         = "验证码错误"
-	NotFoundError     = "查询内容不存在"
+	NotFoundError     = "内容不存在"
 	UnauthorizedError = "无操作权限"
 	DisableError      = "不可用"
 	AlreadyExistError = "已存在相同数据"
 	PasswordError     = "密码错误"
+	UploadError       = "上传错误"
+	JsonParseError    = "JSON解析失败"
 )
