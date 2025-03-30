6a2d45e57c613d4e97555df1369a4f6ec3b0108a
li <513254687@qq.com>
2025-03-31 00:07:58 +0800

    fix: 部分api路径混淆, 题目搜索问题

--- internal/api/problem.go
+++ internal/api/problem.go
@@ -11,7 +11,7 @@ func ProblemRoute(r *gin.RouterGroup, p handle.ProblemHandle, mid []gin.HandlerF
 		//problem.GET("/set/:tag")
 		problem.GET("/total", mid[1], p.Count)               //获取题目总数
 		problem.GET("/:pid", mid[1], p.Detail)               //获取题目详情
-		problem.POST("/", p.List)                            //获取题目列表
+		problem.POST("", p.List)                             //获取题目列表
 		problem.POST("/create", mid[1], mid[2], p.Create)    //题目创建
 		problem.PUT("/update", mid[1], mid[2], p.UpdateInfo) //题目更新
 		problem.DELETE("/:pid", mid[1], mid[3], p.Delete)    //题目删除
--- middleware/cross.go
+++ middleware/cross.go
@@ -9,11 +9,11 @@ import (
 // CrossDomain 跨域
 func CrossDomain() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "SOJ-Access-Token", "SOJ-Refresh-Token"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOrigins:     []string{"*"},
-		MaxAge:           12 * time.Hour,
+		AllowMethods:  []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Length", "Content-Type", "Authorization", "SOJ-Access-Token", "SOJ-Refresh-Token"},
+		ExposeHeaders: []string{"Content-Length"},
+		//AllowCredentials: true,
+		AllowOrigins: []string{"*"},
+		MaxAge:       12 * time.Hour,
 	})
 }
