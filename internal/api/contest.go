7fff64276d880c09a41772f86e77053b4b870d6b
li <513254687@qq.com>
2025-04-23 22:11:32 +0800

    feat: 增加带token的题目列表api

--- internal/api/contest.go
+++ internal/api/contest.go
@@ -6,16 +6,14 @@ import (
 )
 
 func ContestRoute(r *gin.RouterGroup, c handle.ContestHandle, mid []gin.HandlerFunc) {
-	contest := r.Group("contest")
-	contest.POST("", c.GetContestList) //获取近期比赛列表
-	contest.Use(mid[1])
+	contest := r.Group("contest").Use(mid[1])
 	{
 		contest.GET("/:cid", c.GetInfoByID)       //获取比赛详情
 		contest.GET("/u/:uid", c.GetListByUserID) //获取user创建的比赛
-
-		contest.POST("/create", c.CreateContest) //创建比赛
-		contest.PUT("/update", c.UpdateContest)  //更新比赛信息
-		contest.DELETE("/:cid", c.DeleteContest) //删除比赛
-		contest.POST("/rank", c.GetRankingList)  //比赛成绩排行榜
+		contest.POST("", c.GetContestList)        //获取近期比赛列表
+		contest.POST("/create", c.CreateContest)  //创建比赛
+		contest.PUT("/update", c.UpdateContest)   //更新比赛信息
+		contest.DELETE("/:cid", c.DeleteContest)  //删除比赛
+		contest.POST("/rank", c.GetRankingList)   //比赛成绩排行榜
 	}
 }
